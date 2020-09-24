358b3b2e9b34ca916d34d1447504455df909ee8c
Chi Vong <chivongv@gmail.com>
2020-09-24 14:06:39 +0200

    chore: update support for nextjs jest

---
+++ web/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  testPathIgnorePatterns: ['/.next/', '/node_modules/'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+  },
+};
--- web/test-utils.tsx
+++ web/test-utils.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { ThemeProvider } from 'theme-ui';
 import '@testing-library/jest-dom';
-import theme from '../../styles/theme';
+import theme from './styles/theme';
 
 const ThemeWrapper = ({ children }) => {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
