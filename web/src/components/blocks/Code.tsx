e7b71ebe719e1b87fd522c5f6ed6537a682ea76f
Chi Vong <chivongv@gmail.com>
2021-03-01 08:35:29 +0100

    🎨 Minor style update

--- web/src/components/blocks/Code.tsx
+++ web/src/components/blocks/Code.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import styled from '@emotion/styled';
 import { FiCopy } from 'react-icons/fi';
+
 import syntaxTheme from '@styles/syntax-theme';
+import { toCapitalize } from '@utils/string-utils';
 
 const Header = styled('div')`
   display: flex;
@@ -21,8 +23,8 @@ const Header = styled('div')`
   padding: 5px 12px 5px 20px;
 `;
 
-const Title = styled('h3')({
-  fontSize: '1rem',
+const Title = styled('span')({
+  fontSize: '0.875rem',
 });
 
 const Button = styled('button')({
@@ -42,6 +44,7 @@ const Button = styled('button')({
 });
 
 const Text = styled('div')({
+  height: 26,
   padding: 5,
   fontWeight: 700,
   color: '#1fb742',
@@ -65,7 +68,7 @@ const CodeBlock = ({ node }) => {
   return (
     <div>
       <Header>
-        <Title>{language}</Title>
+        <Title>{toCapitalize(language)}</Title>
         {!copied && (
           <Button onClick={handleClick}>
             <FiCopy /> Copy
