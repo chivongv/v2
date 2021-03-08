b9afc0f5b83f8a3a7fc040bfa6a8478b5a029e7e
Chi Vong <chivongv@gmail.com>
2021-03-08 19:55:05 +0100

    ♻️  Refactor for sanity env variables

--- cms/sanity/resolveProductionUrl.js
+++ cms/sanity/resolveProductionUrl.js
@@ -1,6 +1,9 @@
-const previewSecret = 'PreviewSecret'; // Copy the string you used for SANITY_PREVIEW_SECRET
-const projectUrl = 'http://localhost:3000';
+const previewSecret = process.env.SANITY_STUDIO_PREVIEW_SECRET;
+const projectUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://chivongv.se'
+    : 'http://localhost:3000';
 
 export default function resolveProductionUrl(document) {
-  return `${projectUrl}/api/preview?secret=${previewSecret}&slug=${document.slug.current}`;
+  return `${projectUrl}/api/preview?secret=${previewSecret}&type=${document._type}&slug=${document.slug.current}`;
 }
